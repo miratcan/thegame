8fb6b1c42baf24711aebd3ab9f5f194b3538570f
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2023-10-26 17:42:14 +0300

    Daily

--- main.py
+++ main.py
@@ -1,43 +1,32 @@
 import pyxel
-from libs.boxer import Box, FIXED, FILL, VERTICAL, HORIZONTAL
+from libs.boxer import Box, FILL, VERTICAL, HORIZONTAL
 
-screen = Box(
-    name='screen',
-    width=320,
-    height=240,
-    padding=[10, 10, 10, 10]
-)
+screen = Box('screen', 320, 240, direction=VERTICAL)
 
 screen\
-    .add_child(
-        'left_panel',
-        width=100,
-        height=FILL,
-        padding=[10, 10, 10, 10]
-    ).add_sibling(
-        'right_panel',
-        width=FILL,
-        height=FILL,
-        padding=[10, 10, 10, 10],
-        direction=VERTICAL
-    )
-
-for name in ['x', 'y', 'z', 't'][:4]:
-    screen.right_panel.add_child(name, FILL, FILL, margin=[10, 10, 10, 10])
+    .add_child('header', FILL, 22, direction=HORIZONTAL, padding=[5, 0, 0, 0])\
+    .add_sibling('body', FILL, FILL, direction=HORIZONTAL)\
+    .add_sibling('footer', FILL, 16, direction=HORIZONTAL)
 
 
-def render_box(box, color):
-    if box.name == 'y':
-        # import ipdb; ipdb.set_trace()
-        pass
+class Indicator(Box):
+    def __init__(self, name, title, value):
+        self.title = title
+        self.value = value
+        super().__init__(name, FILL, FILL, margin=[0, 5, 0, 5])
 
-    x, y, w, h = box.get_bounding_box(inner_level=0)
-    pyxel.rectb(x, y, w, h, 15)
-    pyxel.text(x+2, y+2, box.name, 15-color)
-    x, y, w, h = box.get_bounding_box(inner_level=3)
-    pyxel.rect(x, y, w, h, color)
-    for child in box.children.values():
-        render_box(child, color + 1)
+    def render(self, x, y, w, h):
+        pyxel.text(x, y, self.title, 8)
+        pyxel.rectb(x, y + 8, w, h - 8, 3)
+        pyxel.rect(x+1, y + 9, (w / 100 * self.value) - 2, h - 10, 4)
+
+
+screen.header.add_child_obj(
+    Indicator('health', 'Health', 30),
+    Indicator('food', 'Food', 22),
+    Indicator('army', 'Army', 85),
+    Indicator('religion', 'Religion', 100)
+)
 
 
 class App:
@@ -50,8 +39,7 @@ class App:
 
     def draw(self):
         pyxel.cls(0)
-
-        render_box(screen, 3)
+        screen._render()
 
 
 App()
