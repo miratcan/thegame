de376ed8795ee1c99ec14046ef34f074818b58a7
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2023-10-26 07:47:24 +0300

    MMM

--- libs/boxer/__init__.py
+++ libs/boxer/__init__.py
@@ -68,7 +68,7 @@ class Box(object):
             result.append(sibling)
         return result
 
-    def get_size_method(self, axis):
+    def _get_size_method(self, axis):
         assert axis in (HORIZONTAL, VERTICAL)
         method_index = {HORIZONTAL: 0, VERTICAL: 1}[axis]
         return self.size_method[method_index]
@@ -78,47 +78,46 @@ class Box(object):
         indexes = [1, 3] if axis == HORIZONTAL else [0, 2]
         return sum(get_values_by_indexes(indexes, values))
 
-    def get_padding(self, axis):
+    def _get_padding_width(self, axis):
         return self._get_axis_width(axis, self.padding)
 
-    def get_border(self, axis):
+    def _get_border_width(self, axis):
         return self._get_axis_width(axis, self.border)
 
-    def get_margin(self, axis):
+    def _get_margin_width(self, axis):
         return self._get_axis_width(axis, self.margin)
 
-    def get_client_size(self, axis):
-        return self.get_content_size(axis) + self.get_padding(axis)
-
-    def get_offset_size(self, axis):
-        return self.get_client_size(axis) + self.get_border(axis)
-
-    def get_full_size(self, axis):
-        return self.get_offset_size(axis) + self.get_margin(axis)
+    def get_size(self, axis, reduce_margin=False, reduce_border=False,
+                 reduce_padding=False):
+        result = self._get_size(axis)
+        if reduce_margin:
+            result -= self._get_padding_width(axis)
+        if reduce_border:
+            result -= self._get_border_width(axis)
+        if reduce_padding:
+            result -= self._get_padding_width(axis)
+        return min(result, 0)
 
     def get_available_space(self, axis):
-        parent_size = self.parent.get_content_size(axis)
+        parent_size = self.parent._get_size(axis)
         non_fill_siblings = filter(
-            lambda b: b.get_size_method(axis) != FILL, self.siblings
+            lambda b: b._get_size_method(axis) != FILL, self.siblings
         )
         size_of_siblings = sum(map(
-            lambda b: b.get_full_size(axis), non_fill_siblings
+            lambda b: b.get_size(axis), non_fill_siblings
         ))
-        space_around = self.get_margin(axis) + \
-            self.get_border(axis) + self.get_padding(axis)
+        space_around = self._get_margin_width(axis) + \
+            self._get_border_width(axis) + self._get_padding_width(axis)
         return parent_size - size_of_siblings - space_around
 
-    def get_content_size(self, axis):
-        size_method = self.get_size_method(axis)
+    def _get_size(self, axis):
+        size_method = self._get_size_method(axis)
         fixed_size_attr = {HORIZONTAL: 'width', VERTICAL: 'height'}[axis]
         if size_method == FIXED:
-            size = getattr(self, fixed_size_attr)
-            padding = self.get_padding(axis)
-            return size - padding
+            return getattr(self, fixed_size_attr)
         elif size_method == HUG:
-            full_size_attr = 'full_' + fixed_size_attr
             return sum([
-                getattr(b, full_size_attr) for b in self.children.values()
+                b._get_size(axis) for b in self.children.values()
             ])
         elif size_method == FILL:
             if not self.parent:
@@ -127,63 +126,32 @@ class Box(object):
             num_of_shareholders = 1
             if self.direction == axis:
                 num_of_shareholders = len(list(filter(
-                    lambda box: box.get_size_method(axis) == FILL,
+                    lambda box: box._get_size_method(axis) == FILL,
                     self.siblings
                 ))) + 1
             return int(available_space / num_of_shareholders)
-        elif size_method == HUG:
-            pass
 
-    def get_start_pos(self, axis):
+    def _get_pos(self, direction):
         if not self.parent:
             return 0
-        index = 3 if axis == HORIZONTAL else 1
         offset = 0
-        if axis == self.parent.direction:
-            offset = sum([sibling.get_full_size(axis)
-                          for sibling in self.previous_siblings_list])
-        return self.parent.get_start_pos(axis) + self.parent.padding[index] + \
-            offset
+        if direction == self.parent.direction:
+            offset = sum([
+                sibling.get_size(axis)
+                for sibling in self.previous_siblings_list
+            ])
+        index = 3 if direction == HORIZONTAL else 1
+        return self.parent._get_pos(axis) + \
+               self.parent.margin[index] + \
+               self.parent.border[index] + \
+               self.parent.padding[index] + \
+               offset
 
-    @property
-    def content_width(self):  # Width for the inner content.
-        return self.get_content_size(HORIZONTAL)
-
-    @property
-    def content_height(self):  # Height for the inner content.
-        return self.get_content_size(VERTICAL)
-
-    @property
-    def client_width(self):  # Width with padding.
-        return self.get_client_size(HORIZONTAL)
-
-    @property
-    def client_height(self):  # Height with padding.
-        return self.get_client_size(VERTICAL)
-
-    @property
-    def offset_width(self):
-        return self.get_offset_size(HORIZONTAL)
-
-    @property
-    def offset_height(self):
-        return self.get_offset_size(VERTICAL)
-
-    @property
-    def full_width(self):
-        return self.get_full_size(HORIZONTAL)
-
-    @property
-    def full_height(self):
-        return self.get_full_size(VERTICAL)
-
-    @property
-    def content_box(self):
-        return (self.get_start_pos(HORIZONTAL) + self.padding[3],
-                self.get_start_pos(VERTICAL) + self.padding[0],
-                self.content_width, self.content_height)
-
-    @property
-    def full_box(self):
-        return (self.get_start_pos(HORIZONTAL), self.get_start_pos(VERTICAL),
-                self.full_width, self.full_height)
+    def get_box(self, reduce_margin=False, reduce_border=False,
+                reduce_padding=False):
+        return (
+            self._get_pos(HORIZONTAL),
+            self._get_pos(VERTICAL),
+            self._get_size(HORIZONTAL),
+            self._get_size(VERTICAL)
+        )
